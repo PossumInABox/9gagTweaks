f03d8fc6fba5fea26c50ee1aa126f67bd62d6cb1
PossumInABox <34094714+PossumInABox@users.noreply.github.com>
2021-03-10 14:21:23 +0100

    set timeout for image replace function remove unnecessary button for video ui function

--- 9gag.user.js
+++ 9gag.user.js
@@ -1,42 +1,41 @@
 // ==UserScript==
-// @name		9gag UI Tweaks
-// @author		PossumInAbox
+// @name			9gag UI Tweaks
+// @author			PossumInABox
 // @namespace		9tweaks
-// @include		http://9gag.com
-// @include		https://*9gag.com*
-// @include		https://*.9gag.com
-// @version		0.1
+// @include			http://9gag.com
+// @include			https://*9gag.com*
+// @include			https://*.9gag.com
+// @version			0.2
 // @updateURL		https://possuminabox.github.io/9gagTweaks/9gag.user.js
 // @downloadURL		https://possuminabox.github.io/9gagTweaks/9gag.user.js
 // @description		Changes UI on 9gag so it won't suck that much anymore
-// @grant		unsafeWindow
-// @run-at		document-end
+// @grant			unsafeWindow
+// @run-at			document-end
 // ==/UserScript==
 
 function replaceImageURLs() {
-	try {
-		let oldSource = document.querySelector(".image-post picture source").getAttribute('srcset')
-		let jpgSource = document.querySelector(".image-post picture img").getAttribute('src')
+	setTimeout(function () {
+		try {
+			let oldSource = document.querySelector(".image-post picture source").getAttribute('srcset')
+			let jpgSource = document.querySelector(".image-post picture img").getAttribute('src')
 
-		if (oldSource.includes(".webp")) {
-			document.querySelector(".image-post picture source").setAttribute('srcset', jpgSource)
+			if (oldSource.includes(".webp")) {
+				document.querySelector(".image-post picture source").setAttribute('srcset', jpgSource)
+			}
+		} catch (e) {
+
 		}
-	} catch (e) {
-
-	}
+	}, 200)
 }
 
- 
+
 function replaceVideoUI() {
- 	setTimeout(function() { 
- 		console.log("dumb controls removed")
- 		document.querySelector('video').setAttribute('controls', '')
- 		document.querySelector('.sound-toggle').style.display = 'none'
- 		console.log("dumb controls removed")
- 	
+	setTimeout(function() {
+		console.log("dumb controls removed")
+		document.querySelector('video').setAttribute('controls', '')
+		document.querySelector('.sound-toggle').style.display = 'none'
 	}, 500);
 }
 
 replaceImageURLs()
 replaceVideoUI()
-document.querySelector('.post-afterbar-meta').innerHTML += '<button onclick="replaceVideoUI()">Remove controls</button>'
